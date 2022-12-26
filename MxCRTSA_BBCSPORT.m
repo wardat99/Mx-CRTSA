0a6dd329862de232fab6ba1e0a61e7da9215c02a
Mohammad Al-Wardat <60725003+wardat99@users.noreply.github.com>
2022-12-26 10:45:39 +0300

    Add files via upload

---
+++ MxCRTSA_BBCSPORT.m
@@ -0,0 +1,30 @@
+clear
+clc
+close all
+
+addpath('dataset','functions');
+load('myBBCSPORT.mat')
+
+mu = 0.3;           %best at  0.3  nmi=0.9893
+lambda=0.02;        %best at  0.02  
+
+for i=1:10
+    
+    fprintf('----Mx-CRTSA start, attempt number %d--------\n', i);
+[Plabel,Timecost(i)] = coregMRTSC(A,mu,lambda,numClust);
+   fprintf('----Mx-CRTSA end, attempt number %d--------\n', i);
+acc(i) = ClusteringMeasure(Plabel, truth);
+        [~, nmi(i),~] = compute_nmi(truth,Plabel);
+        f(i) = compute_f(truth,Plabel);
+        if (min(truth)==0)
+            AR(i)=RandIndex(truth+1,Plabel);
+        else
+            AR(i)=RandIndex(truth,Plabel);
+        end 
+end
+
+fprintf('acc: %.4f    (%.4f)\n' , mean(acc),std(acc));
+fprintf('nmi: %.4f  (%.4f)\n' , mean(nmi), std(nmi));
+fprintf('AR: %.4f   (%.4f)\n' , mean(AR), std(AR));
+fprintf('F-score: %.4f    (%.4f)\n' , mean(f),std(f));
+fprintf('Timecost: %.4f  \n\n' , mean(Timecost));
---
+++ MxCRTSA_CKM.m
@@ -0,0 +1,31 @@
+clear
+close all
+clc
+warning off;
+addpath('dataset','functions');
+load('CKM-PHYSICIANS-INNOVATION.mat')
+load('myCKM.mat')
+
+mu = 0.04;                %best at   0.04 nmi=1.00
+lambda = 0.1;             %best at   0.1
+for i=1:10
+    
+    fprintf('----Mx-CRTSA start, attempt number %d--------\n', i);
+[Plabel,Timecost(i)] = coregMRTSC(A,mu,lambda,numClust);
+   fprintf('----Mx-CRTSA end, attempt number %d--------\n', i);
+acc(i) = ClusteringMeasure(Plabel, truth);
+        [~, nmi(i),~] = compute_nmi(truth,Plabel);
+        f(i) = compute_f(truth,Plabel);
+        if (min(truth)==0)
+            AR(i)=RandIndex(truth+1,Plabel);
+        else
+            AR(i)=RandIndex(truth,Plabel);
+        end 
+end
+
+fprintf('acc: %.4f    (%.4f)\n' , mean(acc),std(acc));
+fprintf('nmi: %.4f  (%.4f)\n' , mean(nmi), std(nmi));
+fprintf('AR: %.4f   (%.4f)\n' , mean(AR), std(AR));
+fprintf('F-score: %.4f    (%.4f)\n' , mean(f),std(f));
+fprintf('Timecost: %.4f  \n\n' , mean(Timecost));
+
