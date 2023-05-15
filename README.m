ab27d34b9786e03aa293a4a3b9d3de0b134ca1f0
Mohammad Al-Wardat <60725003+wardat99@users.noreply.github.com>
2023-05-16 01:55:46 +0300

    Update README.m

--- README.m
+++ README.m
@@ -1,1 +1,36 @@
+# Mx-CRTSA
+The source code for our paper "Discovering Community Structure in Multiplex Networks via a Co-Regularized Robust Tensor-Based Spectral Approach".
 
+Available [here](https://www.mdpi.com/2076-3417/13/4/2514).
+
+# Required toolboxes:
+The MATLAB code for MV-RTSC needs some toolboxes:
+
+[1] Tensor decomposition, Xie, Y., Tao, D., Zhang, W., Liu, Y., Zhang, L., & Qu, Y. (2018). 
+"On unifying multi-view self-representations for clustering by tensor multi-rank minimization."
+
+[2] Tensor Toolbox for MATLAB: <a href="https://www.tensortoolbox.org">www.tensortoolbox.org</a>
+
+# Networks:
+All networks used in our paper are available at [Google Drive](https://drive.google.com/drive/folders/1T8AITuQZCbCB52PELprIVb75d4-6hi20). Each dataset is a mat file containing 2 variables including adjacancy tensor and ground truth of the network. 
+
+# Citation:
+
+If you find this code helpful, please cite the paper as follows:
+
+```
+
+@Article{app13042514,
+AUTHOR = {Al-sharoa, Esraa and Al-wardat, Mohammad and Al-khassaweneh, Mahmood and Bataineh, Ali Al},
+TITLE = {Discovering Community Structure in Multiplex Networks via a Co-Regularized Robust Tensor-Based Spectral Approach},
+JOURNAL = {Applied Sciences},
+VOLUME = {13},
+YEAR = {2023},
+NUMBER = {4},
+ARTICLE-NUMBER = {2514},
+URL = {https://www.mdpi.com/2076-3417/13/4/2514},
+ISSN = {2076-3417},
+DOI = {10.3390/app13042514}
+}
+}
+```
