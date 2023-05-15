db12d3fb0d41f1e10df7eceb1c3a15ea2da11881
Mohammad Al-Wardat <60725003+wardat99@users.noreply.github.com>
2023-05-16 01:50:20 +0300

    Update and rename README.md to README.m

--- README.m
+++ README.m
@@ -11,8 +11,8 @@ The MATLAB code for MV-RTSC needs some toolboxes:
 
 [2] Tensor Toolbox for MATLAB: <a href="https://www.tensortoolbox.org">www.tensortoolbox.org</a>
 
-# Data set:
-All datasets used in our paper are available at [Google Drive](https://drive.google.com/drive/folders/1T8AITuQZCbCB52PELprIVb75d4-6hi20). Each dataset is a mat file containing 2 variables including adjacancy tensor and ground truth of the network. 
+# Networks:
+All networks used in our paper are available at [Google Drive](https://drive.google.com/drive/folders/1T8AITuQZCbCB52PELprIVb75d4-6hi20). Each dataset is a mat file containing 2 variables including adjacancy tensor and ground truth of the network. 
 
 # Citation:
 
